8f588f5750390d3e10c9493aae8fb3497e2efb6e
Jack Pertschuk <jp954@cornell.edu>
2020-01-23 23:28:45 -0800

    .

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // Ensure console.log spits out timestamps
 // firefox reader source: https://hg.mozilla.org/mozilla-central/file/tip/toolkit/components/reader
 require('log-timestamp')
+async = require('async');
 
 // Express
 const app        = require('express')()
@@ -37,29 +38,25 @@ app.post('/', bodyParser, (req, res) => {
 
   console.log('Fetching ' + urls + '...')
 
-  Promise.all(urls.map((url) => {
-    return axios
-        .get(url)
-        .then((response) => {
-        const dom    = new JSDOM(response.data)
-        const parsed = new readability(dom.window.document, {}).parse()
-        let readableDOM = new JSDOM(parsed.content)
-        let paragraphElems = [...readableDOM.window.document.querySelectorAll("p")]
-        let paragraphTexts = paragraphElems.map(elem => {return {text: elem.textContent, url}})
+  axios.all(urls.map(url => axios.get(url, 
+    {
+        transformResponse: [(data) => {
+            const dom    = new JSDOM(data)
+            const parsed = new readability(dom.window.document, {}).parse()
+            let readableDOM = new JSDOM(parsed.content)
+            let paragraphElems = [...readableDOM.window.document.querySelectorAll("p")]
+            let paragraphTexts = paragraphElems.map(elem => {return {text: elem.textContent, url}})
 
-        console.log('Fetched and parsed ' + url + ' successfully')
-        return paragraphTexts.filter(paragraph => paragraph.text.length > 10)
-
-        })
-
-    })).then((values) => {
-        return res
-            .status(200)
-            .send({
-            paragraphs: values.flat(1)
-            })
-            .end()
+            console.log('Fetched and parsed ' + url + ' successfully')
+            return paragraphTexts.filter(paragraph => paragraph.text.length > 10)
+  }]}))).then(axios.spread((...responses) => {
+    return res
+    .status(200)
+    .send({
+        paragraphs: responses.map(response => response.data).flat(1)
     })
+    .end()
+  }))
 })
 
 app.listen(port, () => console.log(`Readability.js server listening on port ${port}!`))
